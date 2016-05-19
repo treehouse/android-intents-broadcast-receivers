aa62124c3a28bc1304294406252b2db260eb9d89
benjakuben <ben.jakuben@gmail.com>
2016-05-19 10:53:34 -0700

    End of stage 4 video 3

--- app/src/main/java/com/teamtreehouse/musicmachine/DownloadIntentService.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/DownloadIntentService.java
@@ -1,11 +1,19 @@
 package com.teamtreehouse.musicmachine;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.teamtreehouse.musicmachine.models.Song;
+
 public class DownloadIntentService extends IntentService {
     private static final String TAG = DownloadIntentService.class.getSimpleName();
+
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 22;
 
     public DownloadIntentService() {
         super("DownloadIntentService");
@@ -14,12 +22,24 @@ public class DownloadIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        String song = intent.getStringExtra(MainActivity.EXTRA_SONG);
-        downloadSong(song);
+        Song song = intent.getParcelableExtra(MainActivity.EXTRA_SONG);
+
+        Notification.Builder builder = new Notification.Builder(this)
+                .setSmallIcon(R.drawable.ic_queue_music_white)
+                .setContentTitle("Downloading")
+                .setContentText(song.getTitle())
+                .setProgress(0, 0, true);
+
+        mNotificationManager =
+                (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+
+        downloadSong(song.getTitle());
     }
 
     private void downloadSong(String song) {
-        long endTime = System.currentTimeMillis() + 10*1000;
+        long endTime = System.currentTimeMillis() + 2*1000;
         while (System.currentTimeMillis() < endTime) {
             try {
                 Thread.sleep(1000);
@@ -28,5 +48,7 @@ public class DownloadIntentService extends IntentService {
             }
         }
         Log.d(TAG, song + " downloaded!");
+
+        mNotificationManager.cancel(NOTIFICATION_ID);
     }
 }
--- app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -80,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //downloadSongs();
-                testIntents();
+                downloadSongs();
+//                testIntents();
             }
         });
 
